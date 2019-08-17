b23a6010aa85b4b19ab80f0dd516c42a907174ac
Mark Carlson <mark.carlson@takeoff.com>
2019-08-16 22:16:31 -0700

    Sets app to initial state for demo

--- src/helperFunctions.test.js
+++
@@ -1,8 +0,0 @@
-import { doubleNumber } from './helperFunctions';
-
-describe('doubleNumber', () => {
-    it('should double a number', () => {
-        const actualResult = doubleNumber(4);
-        expect(actualResult).toEqual(8);
-    })
-})
